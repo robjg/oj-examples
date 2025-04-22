c7fa2fa8895f6ae422125676de2f4545b106a490
Rob Gordon <rob@rgordon.co.uk>
2025-04-22 16:11:02 +0100

    Fixes to When and InputHandler.

--- src/test/java/org/oddjob/userguide/TasksTest.java
+++ src/test/java/org/oddjob/userguide/TasksTest.java
@@ -1,144 +1,141 @@
 package org.oddjob.userguide;
 
+import org.junit.Assert;
 import org.junit.Test;
-
-import java.util.Properties;
-
 import org.oddjob.Oddjob;
 import org.oddjob.OddjobLookup;
+import org.oddjob.OurDirs;
 import org.oddjob.Stateful;
 import org.oddjob.arooa.convert.ArooaConversionException;
 import org.oddjob.arooa.reflect.ArooaPropertyException;
 import org.oddjob.arooa.xml.XMLConfiguration;
 import org.oddjob.input.InputHandler;
-import org.oddjob.input.InputRequest;
 import org.oddjob.state.JobState;
 import org.oddjob.state.ParentState;
 import org.oddjob.state.ServiceState;
 import org.oddjob.tools.ConsoleCapture;
-import org.oddjob.OurDirs;
 import org.oddjob.tools.StateSteps;
 
-import org.junit.Assert;
+import java.util.Properties;
 
 public class TasksTest extends Assert {
 
-	private class OurInputHandler implements InputHandler {
-		
-		@Override
-		public Properties handleInput(InputRequest[] requests) {
-			
-			Properties properties = new Properties();
-			
-			properties.setProperty("some.greeting", "Hello");
-			
-			return properties;
-		}
-	}
-	
+    private static class OurInputHandler implements InputHandler {
+
+        @Override
+        public Session start() {
+
+            return requests -> {
+                Properties properties = new Properties();
+                properties.setProperty("some.greeting", "Hello");
+                return properties;
+            };
+        }
+    }
+
     @Test
-	public void testInputWaitExample() throws InterruptedException, ArooaPropertyException, ArooaConversionException {
-		
-		OurDirs dirs = new OurDirs();
-		
-		Oddjob server = new Oddjob();
-		server.setConfiguration(new XMLConfiguration(
-				dirs.relative("examples/userguide/tasks1.xml")));
-		
-		server.load();
-		
-		Stateful waitJob = new OddjobLookup(server).lookup("wait", Stateful.class);
-		
-		StateSteps waitState = new StateSteps(waitJob);
-		waitState.startCheck(JobState.READY, 
-				JobState.EXECUTING);	
-		
-		Stateful echoJob = new OddjobLookup(server).lookup("echo", Stateful.class);
-		
-		StateSteps echoState = new StateSteps(echoJob);
-		echoState.startCheck(JobState.READY, 
-				JobState.EXECUTING, JobState.COMPLETE);	
-		
-		Thread t = new Thread(server);
-		t.start();
-		
-		waitState.checkWait();
+    public void testInputWaitExample() throws InterruptedException, ArooaPropertyException, ArooaConversionException {
 
-		Oddjob client = new Oddjob();
-		client.setConfiguration(new XMLConfiguration(
-				dirs.relative("examples/userguide/tasks2.xml")));
-		client.setInputHandler(new OurInputHandler());
-		
-		ConsoleCapture console = new ConsoleCapture();
-		try (ConsoleCapture.Close close = console.captureConsole()) {
-			
-	        client.run();
-		
-	        assertEquals(ParentState.COMPLETE, client.lastStateEvent().getState());
-		
-	        echoState.checkWait();
-		
-		}
+        OurDirs dirs = new OurDirs();
 
-		String[] lines = console.getLines();
+        Oddjob server = new Oddjob();
+        server.setConfiguration(new XMLConfiguration(
+                dirs.relative("examples/userguide/tasks1.xml")));
 
-		assertEquals("Hello", lines[0].trim());
-		
-		client.destroy();
-		server.destroy();
-		
-	}
-	
+        server.load();
+
+        Stateful waitJob = new OddjobLookup(server).lookup("wait", Stateful.class);
+
+        StateSteps waitState = new StateSteps(waitJob);
+        waitState.startCheck(JobState.READY,
+                JobState.EXECUTING);
+
+        Stateful echoJob = new OddjobLookup(server).lookup("echo", Stateful.class);
+
+        StateSteps echoState = new StateSteps(echoJob);
+        echoState.startCheck(JobState.READY,
+                JobState.EXECUTING, JobState.COMPLETE);
+
+        Thread t = new Thread(server);
+        t.start();
+
+        waitState.checkWait();
+
+        Oddjob client = new Oddjob();
+        client.setConfiguration(new XMLConfiguration(
+                dirs.relative("examples/userguide/tasks2.xml")));
+        client.setInputHandler(new OurInputHandler());
+
+        ConsoleCapture console = new ConsoleCapture();
+        try (ConsoleCapture.Close close = console.captureConsole()) {
+
+            client.run();
+
+            assertEquals(ParentState.COMPLETE, client.lastStateEvent().getState());
+
+            echoState.checkWait();
+
+        }
+
+        String[] lines = console.getLines();
+
+        assertEquals("Hello", lines[0].trim());
+
+        client.destroy();
+        server.destroy();
+
+    }
+
     @Test
-	public void testTaskExample() throws InterruptedException, ArooaPropertyException, ArooaConversionException {
-		
-		OurDirs dirs = new OurDirs();
-		
-		Oddjob server = new Oddjob();
-		server.setConfiguration(new XMLConfiguration(
-				dirs.relative("examples/userguide/tasks3.xml")));
-		
-		server.load();
-		
-		Stateful waitJob = new OddjobLookup(server).lookup("greeting-service", Stateful.class);
-		
-		StateSteps waitState = new StateSteps(waitJob);
-		waitState.startCheck(ServiceState.STARTABLE, 
-				ServiceState.STARTING, ServiceState.STARTED);	
-		
-		Stateful echoJob = new OddjobLookup(server).lookup("echo", Stateful.class);
-		
-		StateSteps echoState = new StateSteps(echoJob);
-		echoState.startCheck(JobState.READY, 
-				JobState.EXECUTING, JobState.COMPLETE);	
-		
-		Thread t = new Thread(server);
-		t.start();
-		
-		waitState.checkWait();
+    public void testTaskExample() throws InterruptedException, ArooaPropertyException, ArooaConversionException {
 
-		Oddjob client = new Oddjob();
-		client.setConfiguration(new XMLConfiguration(
-				dirs.relative("examples/userguide/tasks4.xml")));
-		client.setInputHandler(new OurInputHandler());
-		
-		ConsoleCapture console = new ConsoleCapture();
-		try (ConsoleCapture.Close close = console.captureConsole()) {
-			
-	        client.run();
-		}
-        
-		assertEquals(ParentState.COMPLETE, client.lastStateEvent().getState());
-        
+        OurDirs dirs = new OurDirs();
+
+        Oddjob server = new Oddjob();
+        server.setConfiguration(new XMLConfiguration(
+                dirs.relative("examples/userguide/tasks3.xml")));
+
+        server.load();
+
+        Stateful waitJob = new OddjobLookup(server).lookup("greeting-service", Stateful.class);
+
+        StateSteps waitState = new StateSteps(waitJob);
+        waitState.startCheck(ServiceState.STARTABLE,
+                ServiceState.STARTING, ServiceState.STARTED);
+
+        Stateful echoJob = new OddjobLookup(server).lookup("echo", Stateful.class);
+
+        StateSteps echoState = new StateSteps(echoJob);
+        echoState.startCheck(JobState.READY,
+                JobState.EXECUTING, JobState.COMPLETE);
+
+        Thread t = new Thread(server);
+        t.start();
+
+        waitState.checkWait();
+
+        Oddjob client = new Oddjob();
+        client.setConfiguration(new XMLConfiguration(
+                dirs.relative("examples/userguide/tasks4.xml")));
+        client.setInputHandler(new OurInputHandler());
+
+        ConsoleCapture console = new ConsoleCapture();
+        try (ConsoleCapture.Close close = console.captureConsole()) {
+
+            client.run();
+        }
+
+        assertEquals(ParentState.COMPLETE, client.lastStateEvent().getState());
+
         echoState.checkWait();
-	
 
-		String[] lines = console.getLines();
 
-		assertEquals("Hello", lines[0].trim());
-		
-		client.destroy();
-		server.destroy();
-		
-	}
+        String[] lines = console.getLines();
+
+        assertEquals("Hello", lines[0].trim());
+
+        client.destroy();
+        server.destroy();
+
+    }
 }
